2549339e408d8c6e069a311cf13789d3ebb3d3db
Gabriel4g <gabrielprogramador11@gmail.com>
2022-07-24 20:04:40 -0500

    add button from like and stop

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const container = document.querySelector("#container");
 const button_generator = document.querySelector("#generator-button");
 const nameText = document.querySelector("#nomes");
 const button_stop = document.querySelector("#stop");
+const button_like = document.querySelector("#like-name");
 
 const names = [
     "Gabriel", "Gustavo", "Julio", "Ismael", "Aquino", "Geovane", "João", "Lucas",
@@ -17,29 +18,36 @@ const namesGenerator = () => {
 
     nameText.innerHTML = names[drawNames];
 
-    /* setTimeout(() => {
-        alert(`Você gerou ${names[drawNames]}`)
-    }, 200) */
-
-    nameText.style.fontSize = "2rem"
-    nameText.style.letterSpacing = "1px"
+    nameText.style.fontSize = "2rem";
+    nameText.style.letterSpacing = "1px";
+    button_like.style.display = "flex";
 }
- 
+
 const automaticGenerator = () => {
-    nameText.innerHTML = ""
+    nameText.innerHTML = "Loading..."
 
     setInterval(() => {
     const drawNames = Math.floor(Math.random() * names.length);
 
     nameText.innerHTML = names[drawNames];
-    }, 2000);
+    }, 2500);
 
     nameText.style.fontSize = "2rem"
     nameText.style.letterSpacing = "1px"
 
-    button_stop.style.display = "block"
+    button_stop.style.display = "flex"
+    button_like.style.display = "flex"
+}
+
+const likeFromName = () => {
+    let likedName = [];
+    alert(`Você gostou de ${nameText.innerHTML}`);
+    
+    likedName.push(nameText.innerHTML);
+
+    console.log(likedName);
 }
 
 const stopAutomatic = () => {
-    location.reload()
+    location.reload();
 }
